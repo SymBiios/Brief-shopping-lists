e86fa229a3406b77a287c0c12e643fdc73040248
symbios <julien.sarkarin@gmail.com>
2025-05-20 11:49:55 +0200

    feat: add remove single item by single item

--- javascript/main.js
+++ javascript/main.js
@@ -1,1 +1,35 @@
 "use strict";
+const itemNameInput = document.querySelector("#item-name");
+const itemQuantityInput = document.querySelector("#item-quantity");
+const addButton = document.querySelector("#add-button");
+const shoppingList = document.querySelector("#shopping-list");
+const clearAllButton = document.querySelector("#clear-all-button");
+const countItem = 0;
+
+function addItem() {
+  const newItem = document.createElement("li");
+  newItem.textContent = `${itemNameInput.value}`;
+
+  const deleteButton = document.createElement("button");
+  deleteButton.textContent = "Supprimer";
+
+  newItem.appendChild(deleteButton);
+  document.getElementById("shopping-list").appendChild(newItem);
+
+  deleteButton.addEventListener("click", function (event) {
+    this.parentElement.remove();
+  });
+}
+
+function clearAllItems() {
+  shoppingList.innerHTML = "";
+}
+
+function increaseQuantity() {}
+
+function decreaseQuantity() {}
+
+addButton.addEventListener("click", addItem);
+clearAllButton.addEventListener("click", clearAllItems);
+
+console.log(shoppingList);
